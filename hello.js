fd0e433dd52c7247781d249af506f1979182b04d
siddharthchavan <siddharthc@alsionschool.org>
2018-01-17 11:14:42 -0800

    made a localhost server

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log('hello world!');
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server is running at localhost:1337');
